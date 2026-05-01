3da14158ba3e2e08cbded6919b1ee39f03fcf8ea
elyknowles <ely.knowles@yahoo.com>
2026-04-30 21:46:49 -0400

    Add files via upload

---
+++ Final/index.js
@@ -0,0 +1,35 @@
+// Ely Knowles, CSC102, Assignment 7.2, 29APR2026
+// below this line is all the functions that make the nav buttons operational
+// location.replace(""); = load and display this html file when said button is clicked
+function boardsPage()
+{
+    location.replace("Boards/boards.html");
+}
+function gamePage()
+{
+    location.replace("Game/game.html");
+}
+function memePage()
+{
+    location.replace("Meme/meme.html");
+}
+function palPage()
+{
+    location.replace("Palindrome/pal.html");
+}
+function shopPage()
+{
+    location.replace("Shopping/shopping.html");
+}
+function soundPage()
+{
+    location.replace("Sound/sound.html");
+}
+function tablePage()
+{
+    location.replace("Tables/table.html");
+}
+function loginPage()
+{
+    location.replace("login.html");
+}
